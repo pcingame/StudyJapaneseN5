28a853f4e7cc44e04404875062213d0c8505c819
pcingame <vipanh246@gmail.com>
2023-06-13 23:43:40 +0700

    refactor sound feature

--- app/src/main/java/com/pc/studyjapanesen5/presentation/alphabet/HiraganaFragment.kt
+++ app/src/main/java/com/pc/studyjapanesen5/presentation/alphabet/HiraganaFragment.kt
@@ -1,18 +1,23 @@
 package com.pc.studyjapanesen5.presentation.alphabet
 
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.pc.studyjapanesen5.R
 import com.pc.studyjapanesen5.base.BaseFragment
 import com.pc.studyjapanesen5.base.recyclerview.SimpleListAdapter
-import com.pc.studyjapanesen5.common.utils.SoundUtils
 import com.pc.studyjapanesen5.databinding.FragmentHiraganaBinding
 import com.pc.studyjapanesen5.databinding.ItemAlphabetBinding
 import com.pc.studyjapanesen5.domain.model.AlphabetModel
+import java.util.Locale
 
 
 class HiraganaFragment :
-    BaseFragment<FragmentHiraganaBinding, AlphabetViewModel>(FragmentHiraganaBinding::inflate) {
+    BaseFragment<FragmentHiraganaBinding, AlphabetViewModel>(FragmentHiraganaBinding::inflate),
+    TextToSpeech.OnInitListener {
     override val viewModel: AlphabetViewModel by viewModels()
+    private lateinit var textToSpeech: TextToSpeech
+
 
     private val hiraganaSingleAdapter by lazy {
         SimpleListAdapter<ItemAlphabetBinding, AlphabetModel>(ItemAlphabetBinding::inflate) { item, _ ->
@@ -50,38 +55,38 @@ class HiraganaFragment :
     }
 
     override fun setupViews() {
+        textToSpeech = TextToSpeech(requireContext(), this)
+
         viewBinding.layoutHiraganaSingle.tvLearn.text =
             getString(R.string.learn, getString(R.string.hiragana))
         viewBinding.layoutHiraganaSingle.rcvSingle.adapter = hiraganaSingleAdapter.apply {
-            onItemClick = { it, _ ->
-                val latin = if (it.latin?.equals("n/m", true) == true) {
-                    "nm"
-                } else it.latin
-                SoundUtils.getFileMp3FromAsset(requireContext(), "$latin.mp3")
+            onItemClick = {item, _ ->
+                textToSpeech.speak(item.hiragana, TextToSpeech.QUEUE_FLUSH, null, null)
             }
+
         }
 
         viewBinding.layoutHiraganaDakuon.rcvSingle.adapter = hiraganaDakuonAdapter.apply {
-            onItemClick = { it, _ ->
-                SoundUtils.getFileMp3FromAsset(requireContext(), "${it.latin}.mp3")
+            onItemClick = {item, _ ->
+                textToSpeech.speak(item.hiragana, TextToSpeech.QUEUE_FLUSH, null, null)
             }
         }
 
         viewBinding.layoutHiraganaCombo.rcvSingle.adapter = hiraganaComboAdapter.apply {
-            onItemClick = { it, _ ->
-                SoundUtils.getFileMp3FromAsset(requireContext(), "${it.latin}.mp3")
+            onItemClick = {item, _ ->
+                textToSpeech.speak(item.hiragana, TextToSpeech.QUEUE_FLUSH, null, null)
             }
         }
 
         viewBinding.layoutHiraganaSmall.rcvSingle.adapter = hiraganaSmallAdapter.apply {
-            onItemClick = { it, _ ->
-                SoundUtils.getFileMp3FromAsset(requireContext(), "${it.latin}.mp3")
+            onItemClick = {item, _ ->
+                textToSpeech.speak(item.hiragana, TextToSpeech.QUEUE_FLUSH, null, null)
             }
         }
 
         viewBinding.layoutHiraganaLongVowel.rcvSingle.adapter = hiraganaLongVowelAdapter.apply {
-            onItemClick = { it, _ ->
-                SoundUtils.getFileMp3FromAsset(requireContext(), "${it.latin}.mp3")
+            onItemClick = {item, _ ->
+                textToSpeech.speak(item.hiragana, TextToSpeech.QUEUE_FLUSH, null, null)
             }
         }
     }
@@ -112,9 +117,24 @@ class HiraganaFragment :
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        SoundUtils.stopMp3()
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = textToSpeech.setLanguage(Locale.JAPAN)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Toast.makeText(requireContext(), "Language not supported", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        } else {
+            Toast.makeText(requireContext(), "Initialization failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (textToSpeech.isSpeaking) {
+            textToSpeech.stop()
+        }
+        textToSpeech.shutdown()
     }
 
 }
--- app/src/main/java/com/pc/studyjapanesen5/presentation/alphabet/KatakanaFragment.kt
+++ app/src/main/java/com/pc/studyjapanesen5/presentation/alphabet/KatakanaFragment.kt
@@ -1,18 +1,22 @@
 package com.pc.studyjapanesen5.presentation.alphabet
 
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.pc.studyjapanesen5.R
 import com.pc.studyjapanesen5.base.BaseFragment
 import com.pc.studyjapanesen5.base.recyclerview.SimpleListAdapter
-import com.pc.studyjapanesen5.common.utils.SoundUtils
 import com.pc.studyjapanesen5.databinding.FragmentKatakanaBinding
 import com.pc.studyjapanesen5.databinding.ItemAlphabetBinding
 import com.pc.studyjapanesen5.domain.model.AlphabetModel
+import java.util.Locale
 
 class KatakanaFragment :
-    BaseFragment<FragmentKatakanaBinding, AlphabetViewModel>(FragmentKatakanaBinding::inflate) {
+    BaseFragment<FragmentKatakanaBinding, AlphabetViewModel>(FragmentKatakanaBinding::inflate), TextToSpeech.OnInitListener {
 
     override val viewModel: AlphabetViewModel by viewModels()
+    private lateinit var textToSpeech: TextToSpeech
+
 
     private val katakanaSingleAdapter by lazy {
         SimpleListAdapter<ItemAlphabetBinding, AlphabetModel>(ItemAlphabetBinding::inflate) { item, _ ->
@@ -54,35 +58,36 @@ class KatakanaFragment :
     }
 
     override fun setupViews() {
+        textToSpeech = TextToSpeech(requireContext(), this)
         viewBinding.layoutKatakanaSingle.tvLearn.text =
             getString(R.string.learn, getString(R.string.katakana))
         viewBinding.layoutKatakanaSingle.rcvSingle.adapter = katakanaSingleAdapter.apply {
-            onItemClick = { it, _ ->
-                SoundUtils.getFileMp3FromAsset(requireContext(), "${it.latin}.mp3")
+            onItemClick = {item, _ ->
+                textToSpeech.speak(item.katakana, TextToSpeech.QUEUE_FLUSH, null, null)
             }
         }
 
         viewBinding.layoutKatakanaDakuon.rcvSingle.adapter = katakanaDakuonAdapter.apply {
-            onItemClick = { it, _ ->
-                SoundUtils.getFileMp3FromAsset(requireContext(), "${it.latin}.mp3")
+            onItemClick = {item, _ ->
+                textToSpeech.speak(item.katakana, TextToSpeech.QUEUE_FLUSH, null, null)
             }
         }
 
         viewBinding.layoutKatakanaCombo.rcvSingle.adapter = katakanaComboAdapter.apply {
-            onItemClick = { it, _ ->
-                SoundUtils.getFileMp3FromAsset(requireContext(), "${it.latin}.mp3")
+            onItemClick = {item, _ ->
+                textToSpeech.speak(item.katakana, TextToSpeech.QUEUE_FLUSH, null, null)
             }
         }
 
         viewBinding.layoutKatakanaSmall.rcvSingle.adapter = katakanaSmallAdapter.apply {
-            onItemClick = { it, _ ->
-                SoundUtils.getFileMp3FromAsset(requireContext(), "${it.latin}.mp3")
+            onItemClick = {item, _ ->
+                textToSpeech.speak(item.katakana, TextToSpeech.QUEUE_FLUSH, null, null)
             }
         }
 
         viewBinding.layoutKatakanaLongVowel.rcvSingle.adapter = katakanaLongVowelAdapter.apply {
-            onItemClick = { it, _ ->
-                SoundUtils.getFileMp3FromAsset(requireContext(), "${it.latin}.mp3")
+            onItemClick = {item, _ ->
+                textToSpeech.speak(item.katakana, TextToSpeech.QUEUE_FLUSH, null, null)
             }
         }
     }
@@ -112,6 +117,26 @@ class KatakanaFragment :
             //  val longVowelKatakanaList = alphabet.take(5)
             katakanaLongVowelAdapter.submitList(alphabet)
         }
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = textToSpeech.setLanguage(Locale.JAPAN)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Toast.makeText(requireContext(), "Language not supported", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        } else {
+            Toast.makeText(requireContext(), "Initialization failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (textToSpeech.isSpeaking) {
+            textToSpeech.stop()
+        }
+        textToSpeech.shutdown()
     }
 
 }
